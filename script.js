ea917b4c406289fb3f66cf3046992386a8d2f72b
richard-hartnell <richard@richardhartnell.com>
2023-05-10 20:30:05 -0700

    Adding proto-fader to style.css

--- script.js
+++ script.js
@@ -26,3 +26,9 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+function folioChange() {
+  console.log("It works");
+}
+
+$('figcaption').addEventListener('click', folioChange());
