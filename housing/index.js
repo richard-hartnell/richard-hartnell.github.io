487d866dfe128206f2d8e58514bb1a1cc4c8b91a
unknown <richard@richardhartnell.com>
2023-06-15 23:53:48 -0700

    Elucidated some variables and added a narrative

--- housing/index.js
+++ housing/index.js
@@ -7,15 +7,25 @@ slider.oninput = function() {
   output.innerHTML = this.value;
 };
 
-let burden;
-let returns;
-let moveInYear;
-let moveOutYear;
-let mortgageLength;
-let monthlyRent;
-let houseValue;
+let mortgageLength = 15;
+let houseValue = slider.value;
+let initialRent = houseValue * 1.7 / 12 / mortgageLength;
 let housemates;
-let rentDoubleRate = 10;
+let marketRentDoubleRate = 10;
 
+class Tenant {
+  constructor(moveInYear, moveOutYear) {
+    this.moveInYear = moveInYear;
+    this.moveOutYear = moveOutYear;
+    this.burden = (moveOutYear - moveInYear) * monthlyRent;
+    this.returns = (moveOutYear - mortgageLength) * (monthlyRent / 2);
+    
+  }
+}
 
-
+// narrative:
+// this tenant moves in on year {year} of the mortage.
+// over {num} years of their tenancy, they pay {subT} in rent.
+// {sub-subT} of that goes into paying off the house. the rest is operating costs.
+// over {num} years after they leave, the LT owes them {sub-subT}.
+// they get paid {sub-subT / years * factor} per year.
