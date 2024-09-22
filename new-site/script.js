076889d0bf46f6ef9bc93d660ddc39b96239f06a
Richard Hartnell <109207464+richard-hartnell@users.noreply.github.com>
2024-09-21 17:43:45 -0700

    Continuing fixes for menu, not done yet

--- new-site/script.js
+++ new-site/script.js
@@ -1,14 +1,10 @@
-const menu = document.getElementById('frontpage');
-const glimpse = document.getElementById('glimpse-wrapper');
-const crew = document.getElementById('crew-wrapper');
-const music = document.getElementById('music-wrapper');
-const reviews = document.getElementById('reviews-wrapper');
+const art = document.getElementById('art-wrapper');
+const tech = document.getElementById('tech-wrapper');
+const strategy = document.getElementById('strategy-wrapper');
    
 const menu1 = document.getElementById('menu1');
 const menu2 = document.getElementById('menu2');
 const menu3 = document.getElementById('menu3');
-const menu4 = document.getElementById('menu4');
-const menu5 = document.getElementById('menu5');
 
 const burgerMenu = document.getElementById('nav-burger');
 const burgerMenuIcon = document.getElementById('burger-icon');
@@ -34,51 +30,26 @@ function showMenu(menuId) {
     
   }
 
-  if (menuId === 'glimpse') {
-    menu.style.display = 'none';
-    glimpse.style.display = 'flex';
-    crew.style.display = 'none';
-    music.style.display = 'none';
-    reviews.style.display = 'none';
+  if (menuId === 'art') {
+    art.style.display = 'block';
+    tech.style.display = 'none';
+    strategy.style.display = 'none';
+    menu1.style.fontWeight = 'bold';
+    menu2.style.fontWeight = 'normal';
+    menu3.style.fontWeight = 'normal';
+  } else if (menuId === 'tech') {
+    art.style.display = 'none';
+    tech.style.display = 'block';
+    strategy.style.display = 'none';
     menu1.style.fontWeight = 'normal';
     menu2.style.fontWeight = 'bold';
     menu3.style.fontWeight = 'normal';
-    menu4.style.fontWeight = 'normal';
-    menu5.style.fontWeight = 'normal'}
-  else if (menuId === 'crew') {
-    menu.style.display = 'none';
-    glimpse.style.display = 'none';
-    crew.style.display = 'block';
-    music.style.display = 'none';
-    reviews.style.display = 'none';
+  } else if (menuId === 'strategy') {
+    art.style.display = 'none';
+    tech.style.display = 'none';
+    strategy.style.display = 'block';
     menu1.style.fontWeight = 'normal';
     menu2.style.fontWeight = 'normal';
     menu3.style.fontWeight = 'bold';
-    menu4.style.fontWeight = 'normal';
-    menu5.style.fontWeight = 'normal';
-  } else if (menuId === 'music') {
-    menu.style.display = 'none';
-    glimpse.style.display = 'none';
-    crew.style.display = 'none';
-    music.style.display = 'block';
-    reviews.style.display = 'none';
-    menu1.style.fontWeight = 'normal';
-    menu2.style.fontWeight = 'normal';
-    menu3.style.fontWeight = 'normal';
-    menu4.style.fontWeight = 'bold';
-    menu5.style.fontWeight = 'normal';
-  } else if (menuId === 'reviews') {
-    menu.style.display = 'none';
-    glimpse.style.display = 'none';
-    crew.style.display = 'none';
-    music.style.display = 'none';
-    crew.style.display = 'none';
-    music.style.display = 'none';
-    reviews.style.display = 'flex';
-    menu1.style.fontWeight = 'normal';
-    menu2.style.fontWeight = 'normal';
-    menu3.style.fontWeight = 'normal';
-    menu4.style.fontWeight = 'normal';
-    menu5.style.fontWeight = 'bold';
   }
 }
